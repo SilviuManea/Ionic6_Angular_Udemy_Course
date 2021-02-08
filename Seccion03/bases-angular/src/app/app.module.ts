b5c981e30d46818a31622b982aea233a0f18bb87
Silviu <silviu.manea.hamzau@outlook.com>
2021-02-08 10:47:07 +0100

    UPDATE-3-22-Routes changed to use RouterLink

--- Seccion03/bases-angular/src/app/app.module.ts
+++ Seccion03/bases-angular/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, ContactComponent, HomeComponent],
+  declarations: [AppComponent, AboutComponent, ContactComponent, HomeComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
