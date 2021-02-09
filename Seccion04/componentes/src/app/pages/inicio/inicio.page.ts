c49c8aa4f190deb400c2db837be1b2bd9e7dd4d3
Silviu <silviu.manea.hamzau@outlook.com>
2021-02-09 20:28:52 +0100

    UPD-4-37-Lists in ionic

--- Seccion04/componentes/src/app/pages/inicio/inicio.page.ts
+++ Seccion04/componentes/src/app/pages/inicio/inicio.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'action-sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'alert',
+      redirectTo: '/alert',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
