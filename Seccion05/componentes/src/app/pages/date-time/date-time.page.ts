55b141e9516ee65ca65e5d52634c56e1c985eecf
Silviu <silviu.manea.hamzau@outlook.com>
2021-02-16 17:12:32 +0100

    UPD-5-50-ion-datetime-p2

--- Seccion05/componentes/src/app/pages/date-time/date-time.page.ts
+++ Seccion05/componentes/src/app/pages/date-time/date-time.page.ts
@@ -8,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class DateTimePage implements OnInit {
   fechaNaci: Date = new Date();
 
+  customYearValues = [2025, 2020, 2016, 2008, 2004, 2000, 1996];
+  customPickerOptions = {
+    buttons: [
+      {
+        text: 'Hola',
+        handler: (event) => {
+          console.log(event);
+        },
+      },
+      {
+        text: 'Mundo',
+        andler: () => {
+          console.log('log!');
+        },
+      },
+    ],
+  };
+
   constructor() {}
 
   ngOnInit() {}
