cc90507928a4ec3cc66c23749165335b3fe69cac
Silviu <silviu.manea.hamzau@outlook.com>
2021-02-24 13:10:46 +0100

    ADD-5-58-service and list page

--- Seccion05/componentes/src/app/pages/inicio/inicio.page.ts
+++ Seccion05/componentes/src/app/pages/inicio/inicio.page.ts
@@ -67,6 +67,11 @@ export class InicioPage implements OnInit {
       name: 'Input Forms',
       redirectTo: '/input',
     },
+    {
+      icon: 'list-outline',
+      name: 'List - Sliding',
+      redirectTo: '/list',
+    },
   ];
 
   constructor() {}
