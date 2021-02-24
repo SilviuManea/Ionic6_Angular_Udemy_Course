d9b2644ac206ab193a917bd7839b05db5b2ab94a
Silviu <silviu.manea.hamzau@outlook.com>
2021-02-24 14:43:41 +0100

    UPD-5-59-ion-item-sliding

--- Seccion05/componentes/src/app/pages/list/list.page.ts
+++ Seccion05/componentes/src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
 
@@ -10,11 +11,29 @@ import { DataService } from 'src/app/services/data.service';
 export class ListPage implements OnInit {
   usuarios: Observable<any>;
 
+  // referenciamos el elemento html por su nombre
+  @ViewChild(IonList) ionList: IonList;
+
   // inyectamos el servicio en el constructor.
   constructor(private dataService: DataService) {}
 
   // nos traemos los usuarios y los guardamos en la variable
   ngOnInit() {
     this.usuarios = this.dataService.getUsuarios();
+  }
+
+  favorite(user: any) {
+    console.log('favorite', user);
+    this.ionList.closeSlidingItems();
+  }
+
+  share(user: any) {
+    console.log('share', user);
+    this.ionList.closeSlidingItems();
+  }
+
+  delete(user: any) {
+    console.log('delete', user.name);
+    this.ionList.closeSlidingItems();
   }
 }
