810bbac11dc72eae3b426ca8a96e5c3c03a2bc82
Silviu <silviu.manea.hamzau@outlook.com>
2021-02-25 11:26:02 +0100

    ADD-5-62-ion-loading

---
+++ Seccion05/componentes/src/app/pages/loading/loading.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.page.html',
+  styleUrls: ['./loading.page.scss'],
+})
+export class LoadingPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  mostrarLoading() {}
+}
