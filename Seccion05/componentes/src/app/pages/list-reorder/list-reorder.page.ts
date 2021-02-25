0c4a980446d9aa419509c73bee031296e4e4a314
Silviu <silviu.manea.hamzau@outlook.com>
2021-02-25 10:33:43 +0100

    UPD-5-60-ion-list-reorder

--- Seccion05/componentes/src/app/pages/list-reorder/list-reorder.page.ts
+++ Seccion05/componentes/src/app/pages/list-reorder/list-reorder.page.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-reorder.page.scss'],
 })
 export class ListReorderPage implements OnInit {
+  personajes: string[] = [
+    'Aquaman',
+    'Superman',
+    'Batman',
+    'Mujer Maravilla',
+    'Flash',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  doReorder(event: any) {
+    console.log(event);
+
+    const itemMover = this.personajes.splice(event.detail.from, 1)[0]; //cortamos el elemento de la posicion que nos indica el evento, y lo guardamos en un array y cogemos la posicion 0.
+    this.personajes.splice(event.detail.to, 0, itemMover); //insertamos el elemento en la posición (event.detail.to) , del array itemMover
+
+    event.detail.complete();
+    console.log(this.personajes);
   }
-
 }
